fc770a699608fa629809326f3a83373ed45ed2bc
SedricOuambo <ouambosedric@gmail.com>
2024-01-17 09:46:53 -0500

    Changement d'extension de page et layout

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Bienvenue sur bvvm-app : Bien Vivre et Vivre Mieux</h1>
+      <h1>Bienvenue sur mvvb-app : Bien Vivre et Vivre Mieux</h1>
     </main>
   )
 }
