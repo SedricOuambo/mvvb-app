bad114f17ab0a06280d767d03e412ddc2c830dcc
Doudougladice <yamewagladice03@gmail.com>
2024-01-23 12:53:55 -0500

    Design du footer

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,6 +6,8 @@ import logo from '@/public/logo mvvb.jpg'
 
 export default function Header() {
     return <header className={styles.header}>
+        {/* UNICONS  */}
+        <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css"></link>
         <div className={styles.title}>
         <Image src={logo} alt="Logo mvvb" />
             <h1>MVVB</h1>
@@ -13,4 +15,5 @@ export default function Header() {
         </div>
         
     </header>
+    
 }
