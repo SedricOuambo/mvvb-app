0b149eabd2440740c8f48b84d3d6cf8a5d01b357
SedricOuambo <ouambosedric@gmail.com>
2024-01-31 00:25:19 -0500

    Bannierre, NavMenu et Header Responsive

--- components/Bannierre.jsx
+++ components/Bannierre.jsx
@@ -3,65 +3,66 @@ import { useState, useEffect } from 'react';
 import styles from './Bannierre.module.css'
 
 export default function Bannierre() {
-    const [time, setTime] = useState({
-      days: '00',
-      hours: '00',
-      minutes: '00',
-      seconds: '00',
-    });
-  
-    useEffect(() => {
-      const eventDate = new Date('2024-04-20T23:59:59');
-  
-      const updateCountdown = () => {
-        const currentDate = new Date();
-        const timeDifference = eventDate - currentDate;
-  
-        const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
-  
-        setTime({
-          days: formatTime(days),
-          hours: formatTime(hours),
-          minutes: formatTime(minutes),
-          seconds: formatTime(seconds),
-        });
-      };
-  
-      const formatTime = (value) => (value < 10 ? `0${value}` : value);
-  
-      // Mettre à jour le chronomètre toutes les secondes
-      const intervalId = setInterval(updateCountdown, 1000);
-  
-      
-      return () => clearInterval(intervalId);
-    }, []);
-  
+  const [time, setTime] = useState({
+    days: '00',
+    hours: '00',
+    minutes: '00',
+    seconds: '00',
+  });
 
-    return (
-    <section className={styles.banner}>
-        <span>MVVB</span>  
-            <div className={styles.Chronometre}>
-                <div className={styles.decopmte}>
-                <div className={styles.time}>{time.days}:</div>
-                <div className={styles.label}>JOURS</div>
-                </div>
-                <div className={styles.decopmte}>
-                <div className={styles.time}>{time.hours}:</div>
-                <div className={styles.label}>HEURES</div>
-                </div>
-                <div className={styles.decopmte}>
-                <div className={styles.time}>{time.minutes}:</div>
-                <div className={styles.label}>MINUTES</div>
-                </div>
-                <div className={styles.decopmte}>
-                <div className={styles.time}>{time.seconds}</div>
-                <div className={styles.label}>SECONDES</div>
-                </div>
-            </div>
-            <p>Mieux Vivre et Vivre Bien</p>
-    </section>
-    );
+  useEffect(() => {
+    const eventDate = new Date('2024-04-20T23:59:59');
+
+    const updateCountdown = () => {
+      const currentDate = new Date();
+      const timeDifference = eventDate - currentDate;
+
+      const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+      setTime({
+        days: formatTime(days),
+        hours: formatTime(hours),
+        minutes: formatTime(minutes),
+        seconds: formatTime(seconds),
+      });
+    };
+
+    const formatTime = (value) => (value < 10 ? `0${value}` : value);
+
+    // Mettre à jour le chronomètre toutes les secondes
+    const intervalId = setInterval(updateCountdown, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (<section className={styles.banner}>
+    <span>Prochaine conférence dans : </span>  
+    <div className={styles.Chronometre}>
+      <div className={styles.sous__chrono}>
+        <div className={styles.decopmte}>
+          <div className={styles.time}>{time.days}</div>
+          <div className={styles.label}>JOURS</div>
+        </div>
+        <div className={styles.decopmte}>
+          <div className={styles.time}>{time.hours}</div>
+          <div className={styles.label}>HEURES</div>
+        </div>
+      </div>
+
+      <div className={styles.sous__chrono}>
+        <div className={styles.decopmte}>
+          <div className={styles.time}>{time.minutes}</div>
+          <div className={styles.label}>MINUTES</div>
+        </div>
+        <div className={styles.decopmte}>
+          <div className={styles.time}>{time.seconds}</div>
+          <div className={styles.label}>SECONDES</div>
+        </div>
+      </div>
+    </div>
+  </section>
+  );
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,14 +1,8 @@
 import MenuNav from './MenuNav'
-import Image from 'next/image'
 import styles from './Header.module.css'
-import logo from '@/public/img/logo.jpg'
 
 export default function Header({setPage}) {
     return <header className={styles.header}>
-        <div className={styles.title}>
-            <Image src={logo} alt="Logo mvvb" />
-            <h1>MVVB</h1>
             <MenuNav setPage={setPage}/>
-        </div>
     </header>
 }
