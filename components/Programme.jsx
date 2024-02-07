96a8f38b3dc16e248c139b8f6191f1decf02e759
ChristinOtniel <sotodonouchristinotniel@gmail.com>
2024-02-07 01:34:34 -0500

    Remplissage de la section programme

--- components/Programme.jsx
+++ components/Programme.jsx
@@ -2,9 +2,36 @@
 import { useState } from "react";
 import styles from './Programme.module.css'
 export default function Programme({children, titre}) {
+    const [hidden, setHidden] = useState(true);
+
+    const programme = () => {
+        setHidden(!hidden);
+    }
     return <section className={styles.section}>
         <h1 className='title'> Programme</h1>
         <div className='container'>
-        </div>
+                <div className="day">
+                    <h2 className="h2">Jour 1 : L'Alimentation</h2>
+                    <ul className="ul">
+                        <li>08:00 - 09:00 : Discours d'ouverture</li>
+                        <li>09:00 - 11:00 : Session 1 - MANGER BON ET BIEN, POUR LE PLAISIR ET LA SANTÉ!</li>
+                        <li>11:10 - 13:00 : Session 2 - LES ALIMENTS : DES ALLIÉS POUR NOTRE IMMUNITÉ!</li>
+                        <li>13:10 - 14:00 : PAUSE CAFE</li>
+                        <li>14:10 - 15:10 : Session 3 - AVEZ-VOUS BOUGÉ AUJOURD’HUI?</li>
+                        <li>15:20 - 16:20 : Session 4 - OPTIMISEZ VOTRE ENTRAÎNEMENT</li>
+                    </ul>
+                </div>
+                <div className="day">
+                    <h2 className="h2">Jour 2 : Comprendre la Psychologie Humaine</h2>
+                    <ul className="ul">
+                    <li>08:00 - 09:00 : Resume des themes aborder le jour precedent</li>
+                        <li>09:00 - 11:00 : Session 1 - LA ROBUSTESSE PSYCHOLOGIQUE</li>
+                        <li>11:10 - 13:00 : Session 2 - INITIATION À LA PLEINE CONSCIENCE!</li>
+                        <li>13:10 - 14:00 : PAUSE CAFE</li>
+                        <li>14:10 - 15:10 : Session 3 - STRESSÉ(E), JE REPRENDS LE CONTRÔLE! STRESSÉ(E), JE PASSE À L’ACTION!</li>
+                        <li>15:20 - 16:20 : Session 4 - LE REPOS EFFICACE : UN PILIER ESSENTIEL POUR MAXIMISER SA PERFORMANCE ET SA SANTÉ</li>
+                    </ul>
+                </div>
+            </div>
     </section>
 }
